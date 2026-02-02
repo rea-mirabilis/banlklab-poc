715c50a51c6cc9d50a2a35c206e5059d9762c771
rea-mirabilis <rearslan7@gmail.com>
2026-02-02 12:01:01 +0300

    Replace link with button for 'Read more' action

--- src/lib/components/cards/NewsCard.svelte
+++ src/lib/components/cards/NewsCard.svelte
@@ -21,12 +21,12 @@
       <slot />
     </div>
     <div class="text-right mt-auto">
-      <a href={base + "/news"} class="text-sm font-bold text-black hover:underline inline-flex items-center">
-        Read More 
+      <button onclick={onClick} class="text-sm font-bold text-black hover:underline inline-flex items-center bg-transparent border-none p-0 cursor-pointer">
+        Read more
         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
         </svg>
-      </a>
+      </button>
     </div>
   </div>
 </div>
